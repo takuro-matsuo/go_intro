d057e2b89ce74afc4218f6b34357aabc84569b1c
takuro <takur819@gmail.com>
2021-01-03 08:23:41 +0900

    gofmt sort

--- main.go
+++ main.go
@@ -6,22 +6,22 @@ import (
 )
 
 func main() {
-    i := []int{5, 3, 2, 7, 8}
-    s := []string{"d", "a", "f"}
-    p := []struct {
-        Name string
-        Age  int
-    }{
-        {"Nancy", 20},
-        {"Vera", 40},
-        {"Mike", 30},
-        {"Bob", 50},
-    }
-    fmt.Println(i, s, p)
-    sort.Ints(i)
-    sort.Strings(s)
-    sort.Slice(p, func(i, j int) bool {return p[i].Name < p[j].Name })
-    fmt.Println(p)
-    sort.Slice(p, func(i, j int) bool {return p[i].Age < p[j].Age })
-    fmt.Println(i, s, p)
+	i := []int{5, 3, 2, 7, 8}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Nancy", 20},
+		{"Vera", 40},
+		{"Mike", 30},
+		{"Bob", 50},
+	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	sort.Strings(s)
+	sort.Slice(p, func(i, j int) bool { return p[i].Name < p[j].Name })
+	fmt.Println(p)
+	sort.Slice(p, func(i, j int) bool { return p[i].Age < p[j].Age })
+	fmt.Println(i, s, p)
 }
