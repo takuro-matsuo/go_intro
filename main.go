d241f95de0f7e98abfba6cd23622693a28579ad8
takuro <takur819@gmail.com>
2021-01-30 10:42:08 +0900

    hmac

--- main.go
+++ main.go
@@ -1,49 +1,37 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
-type T struct{}
-type Person struct {
-    Name        string      `json:"name,omitempty"`
-    Age         int         `json:"age,omitempty"`
-    Nicknames   []string    `json:"nicknames,omitempty"`
-    T           *T          `json:"T,omitempty""`
+// server db
+var DB = map[string]string{
+    "User1Key": "User1Secret", 
+    "User2Key": "User2Secret", 
 }
 
-func (p Person) MarshalJSON() ([]byte, error) {
-    v, err := json.Marshal(&struct{
-        Name string
-    }{
-        Name: "Mr." + p.Name,
-    })
-    return v, err
+//server app
+func Server(apiKey, sign string, data []byte){
+    apiSecret  := DB[apiKey]
+    h := hmac.New(sha256.New, []byte(apiSecret))
+    h.Write(data)
+    expectedHAMC := hex.EncodeToString(h.Sum(nil))
+    fmt.Println(expectedHAMC)
+    fmt.Println(sign == expectedHAMC)
 }
 
-func (p *Person) UnmarshalJSON(b []byte) error {
-    type Person2 struct {
-        Name string
-    }
-    var p2 Person2
-    err := json.Unmarshal(b, &p2)
-    if err != nil {
-        fmt.Println(err)
-    }
-    p.Name = p2.Name + "!"
-    return err
-}
-
-func main() {
-    b := []byte(`{"name":"mike", "age":20, "nicknames":[]}`)
-    var p Person
-    if err := json.Unmarshal(b, &p); err != nil {
-        fmt.Println(err)
-    }
-    fmt.Println(p.Name, p.Age, p.Nicknames)
-
-    v, _ := json.Marshal(p)
-    fmt.Println(string(v))
-
+// client app
+func main(){
+    const apiKey = "User1Key"
+    const apiSecret = "User1Secret"
+    
+    data := []byte("data")
+    h := hmac.New(sha256.New, []byte(apiSecret))
+    h.Write(data)
+    sign := hex.EncodeToString(h.Sum(nil))
+    fmt.Println(sign)
+    Server(apiKey, sign, data)
 }
