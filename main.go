4dd109002e3a15e620e46ff8e8101a7d77f4c7c4
takuro <takur819@gmail.com>
2021-01-30 10:14:55 +0900

    json.Marshal

--- main.go
+++ main.go
@@ -1,34 +1,49 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
-	"time"
 )
-func longProcess(ctx context.Context, ch chan string){
-    fmt.Println("run")
-    time.Sleep(2 * time.Second)
-    fmt.Println("finish")
-    ch <- "result"
-}
-func main() {
-    ch := make(chan string)
-    // ctx := context.Background()
-    // ctx, cancel := context.WithTimeout(ctx, 1 * time.Second)
-    // defer cancel()
-    ctx := context.TODO()
-    go longProcess(ctx, ch)
 
-    CTXLOOP:
-    for {
-        select {
-        case <-ctx.Done():
-            fmt.Println(ctx.Err())
-            break CTXLOOP
-        case <-ch:
-            fmt.Println("success")
-            break CTXLOOP
-        }
+type T struct{}
+type Person struct {
+    Name        string      `json:"name,omitempty"`
+    Age         int         `json:"age,omitempty"`
+    Nicknames   []string    `json:"nicknames,omitempty"`
+    T           *T          `json:"T,omitempty""`
+}
+
+func (p Person) MarshalJSON() ([]byte, error) {
+    v, err := json.Marshal(&struct{
+        Name string
+    }{
+        Name: "Mr." + p.Name,
+    })
+    return v, err
+}
+
+func (p *Person) UnmarshalJSON(b []byte) error {
+    type Person2 struct {
+        Name string
     }
-    fmt.Println("#####################")
+    var p2 Person2
+    err := json.Unmarshal(b, &p2)
+    if err != nil {
+        fmt.Println(err)
+    }
+    p.Name = p2.Name + "!"
+    return err
+}
+
+func main() {
+    b := []byte(`{"name":"mike", "age":20, "nicknames":[]}`)
+    var p Person
+    if err := json.Unmarshal(b, &p); err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(p.Name, p.Age, p.Nicknames)
+
+    v, _ := json.Marshal(p)
+    fmt.Println(string(v))
+
 }
