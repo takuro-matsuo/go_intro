d82896286497bd24badcbfc124b6fba1f5243a75
takuro <takur819@gmail.com>
2021-01-30 11:15:11 +0900

    semaphore

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/spf13/cobra v1.1.1 // indirect
 	go.starlark.net v0.0.0-20210126161401-bc864be25151 // indirect
 	golang.org/x/arch v0.0.0-20210127225635-455c95562d18 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	rsc.io/quote v1.5.2
--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
+	"context"
 	"fmt"
+	"time"
+	"golang.org/x/sync/semaphore"
 )
 
-// server db
-var DB = map[string]string{
-    "User1Key": "User1Secret", 
-    "User2Key": "User2Secret", 
+ var s *semaphore.Weighted = semaphore.NewWeighted(1)
+
+func longProcess(ctx context.Context){
+	isAcquired := s.TryAcquire(1)
+	if !isAcquired{
+		fmt.Println("Could not get locked")
+		return
+	}
+	// if err := s.Acquire(ctx, 1); err != nil{
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	defer s.Release(1)
+	fmt.Println("Wait...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Done")
 }
 
-//server app
-func Server(apiKey, sign string, data []byte){
-    apiSecret  := DB[apiKey]
-    h := hmac.New(sha256.New, []byte(apiSecret))
-    h.Write(data)
-    expectedHAMC := hex.EncodeToString(h.Sum(nil))
-    fmt.Println(expectedHAMC)
-    fmt.Println(sign == expectedHAMC)
-}
-
-// client app
 func main(){
-    const apiKey = "User1Key"
-    const apiSecret = "User1Secret"
-    
-    data := []byte("data")
-    h := hmac.New(sha256.New, []byte(apiSecret))
-    h.Write(data)
-    sign := hex.EncodeToString(h.Sum(nil))
-    fmt.Println(sign)
-    Server(apiKey, sign, data)
+	ctx := context.TODO()
+	go longProcess(ctx)
+	go longProcess(ctx)
+	go longProcess(ctx)
+	time.Sleep(2 * time.Second)
+	go longProcess(ctx)
+	time.Sleep(5 * time.Second)
 }
