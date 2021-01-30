635716bb6b34f4b1a06ac7ded513627b9cfd33ec
takuro <takur819@gmail.com>
2021-01-30 13:29:53 +0900

    init

--- go.mod
+++ go.mod
@@ -19,6 +19,8 @@ require (
 	golang.org/x/arch v0.0.0-20210127225635-455c95562d18 // indirect
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	gopkg.in/go-ini/ini.v1 v1.62.0 // indirect
+	gopkg.in/ini.v1 v1.62.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	rsc.io/quote v1.5.2
 )
--- main.go
+++ main.go
@@ -1,36 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
-	"golang.org/x/sync/semaphore"
+
+	"gopkg.in/ini.v1"
 )
 
- var s *semaphore.Weighted = semaphore.NewWeighted(1)
-
-func longProcess(ctx context.Context){
-	isAcquired := s.TryAcquire(1)
-	if !isAcquired{
-		fmt.Println("Could not get locked")
-		return
-	}
-	// if err := s.Acquire(ctx, 1); err != nil{
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	defer s.Release(1)
-	fmt.Println("Wait...")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Done")
+type ConfigList struct {
+	Port 			int
+	DbName 		string
+	SQLDriver string
 }
 
-func main(){
-	ctx := context.TODO()
-	go longProcess(ctx)
-	go longProcess(ctx)
-	go longProcess(ctx)
-	time.Sleep(2 * time.Second)
-	go longProcess(ctx)
-	time.Sleep(5 * time.Second)
+var Config ConfigList
+
+func init() {
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port : cfg.Section("web").Key("port").MustInt(),
+		DbName : cfg.Section("db").Key("name").MustString("example.sql"),
+		SQLDriver:  cfg.Section("db").Key("driver").String(),
+	}
+}
+
+func main() {
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
 }
